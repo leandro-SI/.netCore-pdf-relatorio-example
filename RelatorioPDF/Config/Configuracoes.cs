90bb6cbc1352ba78a3bbce0d86045d835f3ce4cb
Leandro Cesar <leandro.cesar@globalsys.com.br>
2022-05-17 11:32:32 -0300

    configurações das celulas

---
+++ RelatorioPDF/Config/Configuracoes.cs
@@ -0,0 +1,49 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RelatorioPDF.Config
+{
+    public static class Configuracoes
+    {
+        public static void CriarCelulaTexto(PdfPTable tabela, string texto, int alinhamentoHorz = PdfPCell.ALIGN_LEFT, bool negrito = false, bool italico = false, int tamanhoFonte = 1, int alturaCelula = 25)
+        {
+            tamanhoFonte = 8;
+            var fontBase = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, false);
+
+            int estilo = iTextSharp.text.Font.NORMAL;
+
+            if (negrito && italico)
+            {
+                estilo = iTextSharp.text.Font.BOLDITALIC;
+            }
+            else if (negrito)
+            {
+                estilo = iTextSharp.text.Font.BOLD;
+            }
+            else if (italico)
+            {
+                estilo = iTextSharp.text.Font.ITALIC;
+            }
+
+            var fonteCelula = new iTextSharp.text.Font(fontBase, tamanhoFonte, estilo, BaseColor.Black);
+
+            var bgColor = iTextSharp.text.BaseColor.White;
+            if (tabela.Rows.Count % 2 == 1)
+                bgColor = new BaseColor(0.95F, 0.95F, 0.95F);
+
+            var celula = new PdfPCell(new Phrase(texto, fonteCelula));
+
+            celula.HorizontalAlignment = alinhamentoHorz;
+            celula.VerticalAlignment = PdfPCell.ALIGN_MIDDLE;
+            celula.Border = 0;
+            celula.BorderWidthBottom = 1;
+            celula.FixedHeight = alturaCelula;
+            celula.PaddingBottom = 5;
+            celula.BackgroundColor = bgColor;
+            tabela.AddCell(celula);
+        }
+    }
+}
