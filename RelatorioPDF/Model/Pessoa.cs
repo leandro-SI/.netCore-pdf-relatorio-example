bde6f8465c7c1231f6cee1d77a3507ec04b77d92
Leandro Cesar <leandro.cesar@globalsys.com.br>
2022-05-17 11:28:17 -0300

    modelo

---
+++ RelatorioPDF/Model/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RelatorioPDF.Model
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public string Ativo { get; set; }
+    }
+}
